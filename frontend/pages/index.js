6de93c4044504f9e2bb554c758fe428151047da3
Ochuko Ekrresa <ochuko@fliqpay.com>
2021-10-01 16:43:13 +0100

    Use dotenv

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
-import { ethers } from "ethers";
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 
-import contractABI from "../../artifacts/contracts/WavePortal.sol/WavePortal.json";
+import contractABI from '../../artifacts/contracts/WavePortal.sol/WavePortal.json';
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 export default function Home() {
-  const [currentAccount, setCurrentAccount] = useState("");
+  const [currentAccount, setCurrentAccount] = useState('');
   const [allWaves, setAllWaves] = useState([]);
   const [totalWaves, setTotalWaves] = useState(0);
   const [loading, setLoading] = useState(false);
-
-  const CONTRACT_ADDRESS = "0x6e92BC7487b5DD84056C9444156b16cCEA10CCf3";
 
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -17,17 +17,14 @@ export default function Home() {
 
   const checkIfWalletIsConnected = async () => {
     setLoading(true);
-    const { ethereum } = window;
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
+    if (!window.ethereum) {
+      alert('Make sure you have metamask!');
       setLoading(false);
       return;
-    } else {
-      console.log("We have the ethereum object", ethereum);
     }
 
-    const accounts = await ethereum.request({ method: "eth_accounts" });
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
 
     if (accounts.length) {
       const account = accounts[0];
@@ -43,12 +40,12 @@ export default function Home() {
       );
 
       const count = await wavePortalContract.getTotalWaves();
-      console.log("Found an authorized account:", account);
+      console.log('Found an authorized account:', account);
       setTotalWaves(count.toNumber());
       setLoading(false);
     } else {
       setLoading(false);
-      console.log("No authorized account found");
+      alert('No authorized account found');
     }
   };
 
@@ -57,15 +54,15 @@ export default function Home() {
       const { ethereum } = window;
 
       if (!ethereum) {
-        alert("Get MetaMask!");
+        alert('Get MetaMask!');
         return;
       }
 
       const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
+        method: 'eth_requestAccounts',
       });
 
-      console.log("Connected", accounts[0]);
+      console.log('Connected', accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (error) {
       console.error(error);
@@ -96,6 +93,22 @@ export default function Home() {
         });
 
         setAllWaves(wavesCleaned);
+
+        /**
+         * Listen in for emitter events!
+         */
+        wavePortalContract.on('NewWave', (from, timestamp, message) => {
+          console.log('NewWave', from, timestamp, message);
+
+          setAllWaves(prevState => [
+            ...prevState,
+            {
+              address: from,
+              timestamp: new Date(timestamp * 1000),
+              message: message,
+            },
+          ]);
+        });
       } else {
         console.log("Ethereum object doesn't exist!");
       }
@@ -104,7 +117,16 @@ export default function Home() {
     }
   };
 
-  const wave = async () => {
+  const wave = async e => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const message = formData.get('message');
+
+    if (!message) {
+      alert('Wave at me with a message, please!');
+      return;
+    }
+
     try {
       if (window.ethereum) {
         setLoading(true);
@@ -119,15 +141,27 @@ export default function Home() {
         /*
          * Execute the actual wave from your smart contract
          */
-        const waveTxn = await wavePortalContract.wave("sample message");
-        console.log("Mining...", waveTxn.hash);
+        const waveTxn = await wavePortalContract.wave(message, {
+          gasLimit: 300000,
+        });
+        console.log('Mining...', waveTxn.hash);
 
         await waveTxn.wait();
-        console.log("Mined -- ", waveTxn.hash);
+        console.log('Mined -- ', waveTxn.hash);
 
         let count = await wavePortalContract.getTotalWaves();
+        const waves = await wavePortalContract.getAllWaves();
+
+        let wavesCleaned = waves.map(wave => {
+          return {
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message,
+          };
+        });
 
         setLoading(false);
+        setAllWaves(wavesCleaned);
         setTotalWaves(count.toNumber());
       }
     } catch (error) {
@@ -141,17 +175,26 @@ export default function Home() {
         <div className="header">👋 Hey there!</div>
 
         <div className="bio">
-          I am Ochuko and I worked on self-driving cars so that's pretty cool
-          right? Connect your Ethereum wallet and wave at me!
+          I am Ochuko and I worked on self-driving cars so that's pretty cool right?
+          Connect your Ethereum wallet and wave at me!
         </div>
 
         <div className="waveDisplay">
-          {loading ? "Loading..." : `Total Waves: ${totalWaves}`}
+          {loading ? 'Loading...' : `Total Waves: ${totalWaves}`}
         </div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
+        <form className="waveForm" onSubmit={wave}>
+          <textarea
+            className="textarea"
+            name="message"
+            rows="4"
+            placeholder="Enter message..."
+          ></textarea>
+
+          <button className="waveButton" type="submit">
+            Wave at Me
+          </button>
+        </form>
 
         {!currentAccount && (
           <button className="waveButton" onClick={connectWallet}>
@@ -161,14 +204,7 @@ export default function Home() {
 
         {allWaves.map((wave, index) => {
           return (
-            <div
-              key={index}
-              style={{
-                backgroundColor: "OldLace",
-                marginTop: "16px",
-                padding: "8px",
-              }}
-            >
+            <div key={index} className="message">
               <div>Address: {wave.address}</div>
               <div>Time: {wave.timestamp.toString()}</div>
               <div>Message: {wave.message}</div>
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,19 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
   networks: {
+    development: {
+      url: 'http://127.0.0.1:8545/',
+      accounts: ['0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'],
+    },
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/476xvIN8hvMIHdEpkDhnoSssxVFNW5oA",
-      accounts: [
-        "aea73af13a3c20c265d5d2cb7e6c2e2f4e13288fcf30a5bc77a8b3d7a9b26511",
-      ],
+      url: process.env.STAGING_ALCHEMY_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
