69fa563c4339a909f49eaca3812de5b067968353
Ochuko Ekrresa <ochuko@fliqpay.com>
2021-10-02 18:01:51 +0100

    Update eslint config

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
         ]);
       });
     }
-  }, [wavePortalContract]);
+  }, [wavePortalContract, currentAccount]);
 
   const checkIfWalletIsConnected = async () => {
     setLoading(true);
