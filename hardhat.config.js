73d13f8d91e2dad28a8fbd7eacd3fae1d4bbe3a9
Ochuko Ekrresa <ochuko@fliqpay.com>
2021-10-02 17:46:10 +0100

    Setup local network

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
   networks: {
     development: {
       url: 'http://127.0.0.1:8545/',
-      accounts: ['0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'],
+      accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'],
     },
     rinkeby: {
       url: process.env.STAGING_ALCHEMY_URL,
