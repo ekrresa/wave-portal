57de08c0786f9f4d0839f775902b282866572983
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-11-17 22:51:57 +0100

    App redesign

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,11 @@ module.exports = {
   networks: {
     development: {
       url: 'http://127.0.0.1:8545/',
+      chainId: 1337,
       accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'],
+    },
+    hardhat: {
+      chainId: 1337,
     },
     rinkeby: {
       url: process.env.STAGING_ALCHEMY_URL,
